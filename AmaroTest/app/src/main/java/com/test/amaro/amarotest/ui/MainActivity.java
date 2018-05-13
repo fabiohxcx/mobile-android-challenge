8db2847dfbbbf3b092b241e2d1b0df74bd59a454
Fabio Hideki <hideki.fabio@gmail.com>
2018-05-13 19:56:25 -0300

    refactor main layout

--- AmaroTest/app/src/main/java/com/test/amaro/amarotest/ui/MainActivity.java
+++ AmaroTest/app/src/main/java/com/test/amaro/amarotest/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.test.amaro.amarotest.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +14,7 @@ import com.test.amaro.amarotest.network.ProductsResponse;
 
 import java.util.List;
 
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import retrofit2.Call;
@@ -25,8 +27,13 @@ public class MainActivity extends AppCompatActivity implements Callback<Products
     List<Product> mBestSellersList;
     ProductsResponse mResponse;
 
+    private Toolbar mToolbar;
+
     @BindView(R.id.test)
     TextView tvTest;
+
+    @BindString(R.string.str_all_time_best_sellers)
+    String mAllTimeBestSellers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +41,18 @@ public class MainActivity extends AppCompatActivity implements Callback<Products
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        initToolbar();
+
         mProductsRepository = new ProductsRepository(this);
         mProductsRepository.getBestSellers(this);
 
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        setTitle(mAllTimeBestSellers);
+        mToolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
     }
 
     @Override
