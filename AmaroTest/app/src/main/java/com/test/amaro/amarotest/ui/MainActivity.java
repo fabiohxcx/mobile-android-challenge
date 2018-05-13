f92c177adfe276977d65576b87c088cf6c9e9318
Fabio Hideki <hideki.fabio@gmail.com>
2018-05-13 12:31:08 -0300

    setup environment

---
+++ AmaroTest/app/src/main/java/com/test/amaro/amarotest/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.test.amaro.amarotest.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.test.amaro.amarotest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
